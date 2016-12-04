46a5edde3714e8bbef84fb3162ef22e2c1189739
Mickael <brenoit.mickael@gmail.com>
2016-12-04 14:57:24 +0100

    TP3 - 1 - Apprentissage de tables de multiplication

---
+++ M1/TechnologieWEB/TP3/tablemultcontroller.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var Controller = Controller || {};
+
+(function (exports){
+    
+    function onLoad(){
+        
+    }
+    
+    function unLoad(){
+        
+    }
+    
+    window.addEventListener("load", onLoad, false);
+    window.addEventListener("unload", unLoad, false);
+    
+})(Controller);
---
+++ M1/TechnologieWEB/TP3/tablemultview.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var View = View || {};
+
+(function (exports){
+    
+    function onLoad(){
+        
+    }
+    
+    function unLoad(){
+        
+    }
+    
+    window.addEventListener("load", onLoad, false);
+    window.addEventListener("unload", unLoad, false);
+    
+})(View);
