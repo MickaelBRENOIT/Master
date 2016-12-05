1e8e66d50edce241ef0f8be312d8ce6aace052bf
Mickael <brenoit.mickael@gmail.com>
2016-12-06 00:43:08 +0100

    TP3 - 3 - 2/4 - Calcul et affichage des résultats

--- M1/TechnologieWEB/TP3/tablemultdata.js
+++ M1/TechnologieWEB/TP3/tablemultdata.js
@@ -4,8 +4,8 @@ var Data = Data || {};
 
 (function (exports){
     
-    //contient le numéro de la table de multiplication actuelle
-    var tableNumber;
+    //contient le numéro de la table de multiplication actuelle (à remplacer)
+    /*var tableNumber;*/
     
     function onLoad(){
         
@@ -15,12 +15,73 @@ var Data = Data || {};
         
     }
     
-    //met à jour le numéro de la table de multiplication actuelle
-    function changeTableNumber(num){
+    //met à jour le numéro de la table de multiplication actuelle (à remplacer)
+    /*function changeTableNumber(num){
         tableNumber = num;
+    }*/
+    
+    //"classe" représentant une table de multiplication
+    //son constructeur prend en paramètres le numéro de la table et le nombre de résultats à calculer
+    //l'objet stocke ces données et le tableau contenant les résultats des multiplications du numéro par la ligne correspondante
+    
+    function MultiplicationTable(tableNumber, nbresults){
+        nbresults = nbresults || 10;
+        this._nbresults = nbresults;
+        this._tableNumber = tableNumber;
+        this._values = [];
+        for(int i=1; i < nbresults+1; i++){
+            this._values.push(i*this._tableNumber);
+        }
+        
+        /*if (!(this instanceof MultiplicationTable)) {
+            return new MultiplicationTable (nbresults, tableNumber);
+        }*/
+    }
+    
+    //renvoie le numéro de la table
+    MultiplicationTable.prototype.number = function(){
+        return this._tableNumber;        
+    };
+    
+    //revoie la valeur numéro "i" 
+    MultiplicationTable.prototype.value = function(i){
+        return this._values[i-1];    
+    };
+    
+    //renvoie le tableau des valeurs
+    MultiplicationTable.prototype.values = function(){
+        return this._values.slice(0);    
+    };
+    
+    exports.MultiplicationTable = MultiplicationTable;
+    
+    //le remplacement
+    var currentMultiplicationTable = null;
+    
+    //le remplacement
+    function changeTableNumber(num){
+        currentMultiplicationTable = new MultiplicationTable(num);
+    }
+    
+    //renvoie le numéro de la table
+    function tableNumber(){
+        return currentMultiplicationTable.number();
+    }
+    
+    //renvoie la valeur numéro "i"
+    function tableValue(i){
+        return currentMultiplicationTable.value(i);
+    }
+    
+    //renvoie le tableau des valeurs
+    function tableValues(){
+        return currentMultiplicationTable.values();
     }
     
     exports.changeTableNumber = changeTableNumber;
+    exports.tableNumber = tableNumber;
+    exports.tableValue = tableValue;
+    exports.tableValues = tableValues;
     
     window.addEventListener("load", onLoad, false);
     window.addEventListener("unload", unLoad, false);
