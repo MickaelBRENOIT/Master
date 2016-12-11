db9b12836edbf3cbd80ea96401be8247ab3b5383
Mickael <brenoit.mickael@gmail.com>
2016-12-11 10:53:27 +0100

    Ajout de la fonction qui permet de trouver les coordonnées minimales et maximales.

--- M1/ModelisationDeTerrains/conversion3d/main.cpp
+++ M1/ModelisationDeTerrains/conversion3d/main.cpp
@@ -15,10 +15,12 @@
 #include <fstream>
 #include <string>
 #include <climits>
+#include <windows.h>
 
 using namespace std;
 
-void display(ifstream &file){
+void display(char* f){
+	ifstream file(f);
     string str; 
     while (getline(file, str, ' '))
     {
@@ -26,8 +28,9 @@ void display(ifstream &file){
     }
 }
 
-void convertTxtToObj(ifstream &input){
-    ofstream output("terrain.obj");
+void convertTxtToObj(char* in){
+	ifstream input(in);
+    ofstream output("SDDv2.obj");
     string str;
     if(output.is_open()){
         while(getline(input, str)){
@@ -38,7 +41,8 @@ void convertTxtToObj(ifstream &input){
     }
 }
 
-void convertTxtIToObj(ifstream &file){
+void convertTxtIToObj(char* f){
+	ifstream file(f);
     ofstream output("terrainI.obj");
     string str; 
     int cpt=0;
@@ -61,61 +65,77 @@ void convertTxtIToObj(ifstream &file){
     }
 }
 
-void findXminXmaxYminYmax(ifstream &input){
-    //ofstream output("terrainAvecCarre.obj");
+void findXminXmaxYminYmax(char* in){
+	ifstream input(in);
+    ofstream output;
     string str;
     int cpt=0;
+    const double vn = 0.2, vt = 0.5;
     double Xmin=LONG_MAX, Ymin=LONG_MAX, Xmax=LONG_MIN, Ymax=LONG_MIN, Zmin=LONG_MAX;
-    //if(output.is_open()){
-        while(getline(input, str, ' '))
-        {
-                      
-            cpt = cpt + 1;
-            cout << cpt << " + " << str << '\n'; 
-            
-            //colonne des X
-            double temp = atof(str.c_str());
-            //sscanf_s(str.c_str, " caractère l'espace ", &a, &b...)
-            if(cpt==1){
-                //cout << "x:" << str << "\n";
-                if(temp<Xmin)
-                    Xmin = temp;
-                if(temp>Xmax)
-                    Xmax = temp;
-            }else if(cpt==2){
-                //cout << "y:" << str << "\n";
-                if(temp<Ymin)
-                    Ymin = temp;
-                if(temp>Ymax)
-                    Ymax = temp;
-            }else if(cpt==3){
-                //cout << "z:" << str << "\n";
-                if(temp<Zmin)
-                    Zmin = temp;
-            }else if (cpt>=6){
-                cpt=0;
-                str.clear();
-            }       
-        }
+    double x,y,z,r,g,b;
+    if(input.is_open()){
+    	while(!input.eof()){
+    		input >> x >> y >> z >> r >> g >> b;
+    		if(x<Xmin)
+    			Xmin = x;
+    		if(x>Xmax)
+    			Xmax = x;
+    		if(y<Ymin)
+    			Ymin = y;
+    		if(y>Ymax)
+    			Ymax = y;
+    		if(z<Zmin)
+    			Zmin = z;
+		}
+	}
+	
+	input.close();
+	
+	output.open(in, ios::app);
+	
+	output << Xmin << " " << Ymin << " " << Zmin << " 0 0 0" << endl;
+	output << Xmax << " " << Ymin << " " << Zmin << " 0 0 0" << endl;
+	output << Xmax << " " << Ymax << " " << Zmin << " 0 0 0" << endl;
+	output << Xmin << " " << Ymax << " " << Zmin << " 0 0 0" << endl;
+	
+	output.close();
+	
+	convertTxtToObj(in);
+	
+	cout << "XMin=" << Xmin << "\n";
+        cout << "XMax=" << Xmax << "\n";
+        cout << "YMin=" << Ymin << "\n";
+        cout << "YMax=" << Ymax << "\n";
+        cout << "ZMin=" << Zmin << "\n";
     
-    cout << "XMin=" << Xmin << "\n";
-    cout << "XMax=" << Xmax << "\n";
+    Sleep(5000);
     
-    cout << "YMin=" << Ymin << "\n";
-    cout << "YMax=" << Ymax << "\n";
+    ofstream face;
     
-    cout << "ZMin=" << Zmin << "\n";
+    face.open("SDDv2.obj", ios::app);
+    
+    output << "f " << "-4 " << "-3 " << "-2 " << "-1 ";
+    
+    /*output << Xmin << " " << Ymin << " " << Zmin << " 0 0 0" << endl;
+	output << Xmax << " " << Ymin << " " << Zmin << " 0 0 0" << endl;
+	output << Xmax << " " << Ymax << " " << Zmin << " 0 0 0" << endl;
+	output << Xmin << " " << Ymax << " " << Zmin << " 0 0 0" << endl;*/
+	
+	face.close();
+    
+		
+}
     
     
     /*}else{
         cout << "Le fichier n'a pas pu être ouvert";
     }*/
-}
 
 int main(int argc, char** argv) {
-    ifstream file("test.txt");
-    findXminXmaxYminYmax(file);
-    //display(file);
+    findXminXmaxYminYmax("SDDv2.txt");
+    //display("SDDv2.txt");
+    //convertTxtToObj("SDDv2.txt");
+    //convertTxtIToObj(file);
     
 }
 
