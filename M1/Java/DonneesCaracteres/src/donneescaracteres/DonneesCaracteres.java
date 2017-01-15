89a05b49868aac1c03b892f365d434438182f7e0
Mickael <brenoit.mickael@gmail.com>
2017-01-15 16:30:32 +0100

    Programme données <--> caractères

---
+++ M1/Java/DonneesCaracteres/src/donneescaracteres/DonneesCaracteres.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package donneescaracteres;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mikael
+ */
+public class DonneesCaracteres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        /*try(Scanner in = new Scanner(new FileInputStream("doubles")); DataOutputStream out = new DataOutputStream(new FileOutputStream("data"))){
+            while(in.hasNextDouble()){
+                out.writeDouble(in.nextDouble());
+            }
+        }*/
+        
+        try(DataInputStream in = new DataInputStream(new FileInputStream("data")); FileWriter out = new FileWriter(new File("double2"))){
+            int c;
+            String s = "";
+            while((c = in.read()) != -1){
+                s += (char) c;
+            }
+            out.write(s);
+        }
+        
+    }
+    
+}
